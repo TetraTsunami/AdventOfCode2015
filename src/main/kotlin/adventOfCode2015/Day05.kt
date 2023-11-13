35bc20aa55b709dfaeb562f068dfcd7ff6dab7c0
Tsuni <akamitsunami@gmail.com>
2023-11-13 10:49:04 -0600

    Day 5

---
+++ src/main/kotlin/adventOfCode2015/Day05.kt
@@ -0,0 +1,28 @@
+package adventOfCode2015
+import util.*
+
+@Suppress("unused")
+class Day05(input: String) : Day(input) {
+    override fun solve(): Result {
+        var total = 0
+        for (line in lines) {
+            if (line.matches(Regex(".*(ab|cd|pq|xy).*"))) {
+                continue
+            }
+            if (line.matches(Regex(".*[aeiou].*[aeiou].*[aeiou].*"))) {
+                if (line.matches(Regex(".*(.)\\1.*"))) {
+                    total++
+                }
+            }
+        }
+        var total2 = 0
+        for (line in lines) {
+            if (line.matches(Regex(".*(..).*\\1.*"))) {
+                if (line.matches(Regex(".*(.).\\1.*"))) {
+                    total2++
+                }
+            }
+        }
+        return Result(total, total2)
+    }
+}
